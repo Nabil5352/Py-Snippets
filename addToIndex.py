b0cc3bbfe43f1f50c74fe312a64eecb04d59077b
Nabil5352 <nabil@tgalimited.com>
2018-09-10 14:56:45 +0600

    indexing url based on keyword

---
+++ addToIndex.py
@@ -0,0 +1,45 @@
+# Define a procedure, add_to_index,
+# that takes 3 inputs:
+
+# - an index: [[<keyword>,[<url>,...]],...]
+# - a keyword: String
+# - a url: String
+
+# If the keyword is already
+# in the index, add the url
+# to the list of urls associated
+# with that keyword.
+
+# If the keyword is not in the index,
+# add an entry to the index: [keyword,[url]]
+
+index = []
+
+def add_to_index(index,keyword,url):
+    if len(index) > 0:
+        counter = 0
+        urlFound = False
+        for i in index:
+            if i[counter] == keyword:
+                for j in i[counter+1]:
+                    if j == url:
+                        urlFound = True
+                        break
+                    
+                if not urlFound:
+                    i[counter+1].append(url)
+                    
+                urlFound = False
+                break
+            else:
+                index.append([keyword,[url]])
+    else:
+        index.append([keyword,[url]])
+
+
+#Test Cases
+add_to_index(index,'udacity','http://udacity.com')
+add_to_index(index,'computing','http://acm.org')
+add_to_index(index,'udacity','http://npr.org')
+print index
+#>>> [['udacity', ['http://udacity.com', 'http://npr.org']],['computing', ['http://acm.org']]]
