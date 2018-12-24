508e1a5baad4deca7e2f3f4e5ce8b217e5a234a9
Nabil5352 <nabil@tgalimited.com>
2018-12-24 14:52:25 +0600

    crawler updatye

--- crawler.py
+++ crawler.py
@@ -1,15 +1,3 @@
-# The current index includes a url in the list of urls
-# for a keyword multiple times if the keyword appears
-# on that page more than once.
-
-# It might be better to only include the same url
-# once in the url list for a keyword, even if it appears
-# many times.
-
-# Modify add_to_index so that a given url is only
-# included once in the url list for a keyword,
-# no matter how many times that keyword appears.
-
 def add_to_index(index, keyword, url):
     for entry in index:
         if entry[0] == keyword:
@@ -23,25 +11,25 @@ def get_page(url):
     try:
         if url == "http://www.udacity.com/cs101x/index.html":
             return '''<html> <body> This is a test page for learning to crawl!
-<p> It is a good idea to
-<a href="http://www.udacity.com/cs101x/crawling.html">
-learn to crawl</a> before you try to
-<a href="http://www.udacity.com/cs101x/walking.html">walk</a> or
-<a href="http://www.udacity.com/cs101x/flying.html">fly</a>.</p></body>
-</html>'''
+                    <p> It is a good idea to
+                    <a href="http://www.udacity.com/cs101x/crawling.html">
+                    learn to crawl</a> before you try to
+                    <a href="http://www.udacity.com/cs101x/walking.html">walk</a> or
+                    <a href="http://www.udacity.com/cs101x/flying.html">fly</a>.</p></body>
+                    </html>'''
 
         elif url == "http://www.udacity.com/cs101x/crawling.html":
             return '''<html> <body> I have not learned to crawl yet, but I am
-quite good at  <a href="http://www.udacity.com/cs101x/kicking.html">kicking</a>.
-</body> </html>'''
+                    quite good at  <a href="http://www.udacity.com/cs101x/kicking.html">kicking</a>.
+                    </body> </html>'''
 
         elif url == "http://www.udacity.com/cs101x/walking.html":
             return '''<html> <body> I cant get enough
-<a href="http://www.udacity.com/cs101x/index.html">crawling</a></body></html>'''
+                    <a href="http://www.udacity.com/cs101x/index.html">crawling</a></body></html>'''
 
         elif url == "http://www.udacity.com/cs101x/flying.html":
             return '''<html>
-<body>The magic words are Squeamish Ossifrage!</body></html>'''
+                    <body>The magic words are Squeamish Ossifrage!</body></html>'''
     except:
         return ""
     return ""
@@ -78,10 +66,15 @@ def crawl_web(seed):
     while tocrawl:
         page = tocrawl.pop()
         if page not in crawled:
+            print index
+            print "BEFORE"
             content = get_page(page)
             add_page_to_index(index, page, content)
+            print index
+            print "AFTER"
             union(tocrawl, get_all_links(content))
             crawled.append(page)
+        
     return index
 
 def add_page_to_index(index, url, content):
@@ -95,6 +88,6 @@ def lookup(index, keyword):
             return entry[1]
     return None
 
-#index = crawl_web("http://www.udacity.com/cs101x/index.html")
+index = crawl_web("http://www.udacity.com/cs101x/index.html")
 #print lookup(index,"is")
 #>>> ['http://www.udacity.com/cs101x/index.html']
